6a6db8b86220e66c3cd3153fe97b04e80bcfc700
Pete <yau17914@uvg.edu.gt>
2020-02-24 10:38:10 -0600

    Initial commit

---
+++ Lab5_i2c_master.X/i2c_master.c
@@ -0,0 +1,16 @@
+/*
+ * File:   i2c_master.c
+ * Author: Peter
+ *
+ * Created on February 24, 2020, 10:34 AM
+ */
+
+
+#include <xc.h>
+#include <stdint.h>
+#include <stdio.h>
+#include "LCDv1.h"
+
+void main(void) {
+    return;
+}
